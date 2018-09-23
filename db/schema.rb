0ae038381aa2fd77c6f127e6419a99bf71d2a90a
pnam2311 <pnam2311@gmail.com>
2018-09-23 16:06:04 +0700

    show users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_055224) do
+ActiveRecord::Schema.define(version: 2018_09_23_082718) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2018_09_23_055224) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "phone_number"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
